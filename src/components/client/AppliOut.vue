14e796588b1db16dc581c2ed98a7fe08b806b276
wenbo <598980528@qq.com>
2020-07-18 18:34:07 +0800

    刘根延1.0

--- src/components/client/AppliOut.vue
+++ src/components/client/AppliOut.vue
@@ -1,3 +1,102 @@
 <template>
-    <div>appliout</div>
+ <div>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+  <el-form-item label="库存编码" prop="goods_num">
+    <el-input v-model="ruleForm.goods_num"></el-input>
+  </el-form-item>
+  <el-form-item label="出库仓库" prop="depot_num">
+    <el-select v-model="ruleForm.depot_num" placeholder="请选择活动区域">
+      <el-option label="一号仓库" value="0"></el-option>
+      <el-option label="二号仓库" value="1"></el-option>
+    </el-select>
+  </el-form-item>
+  <el-form-item label="出库时间" required>
+    <el-col :span="11">
+      <el-form-item prop="outbound_time">
+        <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.outbound_time" style="width: 100%;"></el-date-picker>
+      </el-form-item>
+    </el-col>
+    <el-col class="line" :span="2">-</el-col>
+    <el-col :span="11">
+      <el-form-item prop="outbound_time2">
+        <el-time-picker placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
+      </el-form-item>
+    </el-col>
+  </el-form-item>
+
+
+ <el-form-item label="姓名" prop="user_name">
+    <el-input v-model="ruleForm.user_name"></el-input>
+  </el-form-item>
+
+   <el-form-item label="电话号码" prop="user_tel">
+    <el-input v-model="ruleForm.user_tel"></el-input>
+  </el-form-item>
+
+  <el-form-item label="备注" prop="user_ps">
+    <el-input type="textarea" v-model="ruleForm.user_ps"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="submitForm('ruleForm')">发布申请</el-button>
+    <el-button @click="resetForm('ruleForm')">重置</el-button>
+  </el-form-item>
+</el-form>
+</div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        ruleForm: {
+          goods_num: '',
+          depot_num: '',
+          outbound_time: '',
+          outbound_time2: '',
+          user_name: '',
+          user_tel: '',
+          user_ps: ''
+        },
+        rules: {
+          goods_num: [
+            { required: true, message: '请输入库存编码', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ],
+          depot_num: [
+            { required: true, message: '请选择仓库', trigger: 'change' }
+          ],
+          outbound_time: [
+            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+          ],
+          outbound_time2: [
+            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+          ],
+          user_name: [
+            { required: true, message: '请输入您的姓名', trigger: 'change' }
+          ],
+          user_tel: [
+            { required: true, message: '请输入您的电话号码', trigger: 'change' }
+          ],
+          user_ps: [
+            { required: false, message: '请填写活动形式', trigger: 'blur' }
+          ]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+
+
+        this.$http.post("url",this.ruleForm).then(res => {
+         this.$message({
+          message: " 申请成功",
+           type: "success"
+         });
+      })
+       
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
--- src/components/client/AppliState.vue
+++ src/components/client/AppliState.vue
@@ -1,3 +1,137 @@
 <template>
-    <div>客户购买信息</div>
+  <el-table
+    :data="tableData"
+    border
+    style="width: 100%"
+    :row-class-name="tableRowClassName"
+    :default-sort = "{prop: 'date', order: 'descending'}"
+    >
+    <el-table-column
+      prop="inbound_time"
+      label="申请时间"
+      sortable
+      width="260">
+    </el-table-column>
+    <el-table-column
+      prop="goods_name"
+      label="商品名字"
+      width="180">
+    </el-table-column>
+      <el-table-column
+      prop="goods_count"
+      label="商品数量"
+      sortable
+      width="180">
+    </el-table-column>
+     <el-table-column
+      prop="check"
+      label="申请状态"
+      :formatter="formatter">
+    </el-table-column>
+   
+  </el-table>
 </template>
+<style>
+  .el-table .warning-row {
+    background: rgb(219, 93, 71);
+    color:rgb(54, 52, 52)
+  }
+
+  .el-table .success-row {
+    background: #77da41;
+    color:rgb(54, 52, 52)
+  }
+  .el-table .audit-row{
+       background: #eaec53;
+    color:rgb(54, 52, 52)
+  }
+ 
+</style>
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [{
+          inbound_time: '2016-05-02',
+          goods_name: '老干妈',
+          goods_count: 612,
+          check_situation:'0',
+          check:'审核中'
+        }, {
+         inbound_time: '2017-05-02',
+          goods_name: '干脆面',
+          goods_count: 100,
+          check_situation:'1',
+          check: '通过'
+        }, {
+         inbound_time: '2018-05-02',
+          goods_name: '巧克力',
+          goods_count: 30,
+          check_situation:'1',
+          check: '通过'
+        }, 
+        {
+         inbound_time: '2017-05-02',
+          goods_name: '干脆面',
+          goods_count: 100,
+          check_situation:'1',
+          check: '通过'
+        }, {
+         inbound_time: '2018-05-02',
+          goods_name: '巧克力',
+          goods_count: 30,
+          check_situation:'1',
+           check: '通过'
+        },
+        {
+         inbound_time: '2017-05-02',
+          goods_name: '干脆面',
+          goods_count: 100,
+          check_situation:'1',
+          check: '通过'
+        }, {
+         inbound_time: '2018-05-02',
+          goods_name: '巧克力',
+          goods_count: 30,
+          check_situation:'1',
+          check: '通过'
+        },
+        {
+         inbound_time: '2017-05-02',
+          goods_name: '干脆面',
+          goods_count: 100,
+          check_situation:'1',
+           check: '通过'
+        }, {
+         inbound_time: '2018-05-02',
+          goods_name: '巧克力',
+          goods_count: 30,
+          check_situation:'1',
+          check: '通过'
+        },{
+          inbound_time: '2019-05-02',
+          goods_name: '伏特加',
+          goods_count: 20,
+          check_situation:'2',
+           check: '未通过'
+        }]
+      }
+    },
+    methods: {
+      formatter(row, column) {
+        return row.check;
+      },
+      tableRowClassName({row, check_situation}) {
+        if (row.check_situation == 2) {
+          return 'warning-row';
+        } else if (row.check_situation == 1) {
+          return 'success-row';
+        }else {
+            return 'audit-row';
+        }
+        return '';
+   }
+   
+   }
+  }
+</script>
